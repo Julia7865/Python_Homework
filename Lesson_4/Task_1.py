ccb5c9a5b5c05b6b2e88ecadfcfc7b9413620ee4
Julia K <julia.k.v075@jmail.com>
2022-12-16 20:51:55 +0400

    Lesson 4

--- Lesson_4/Task_1.py
+++ Lesson_4/Task_1.py
@@ -4,12 +4,35 @@
 
 # Пример:
 # если k = 2, то многочлены могут быть => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
+
 import random
 
 n = int(input('Введите степень: '))
 
-x = ''
-for k in range(n, -1, -1):
-    x += f'{random.randint(0, 100)}*x**{k} + '
-x += '= 0'
-print(x)
+def polynominal(num, min, max):
+    result = ''
+    for k in range(num, 1, -1):
+        koefficient = random.randint(min, max)
+        if koefficient != 0:
+            result += f'{koefficient}*x**{k} + '
+    penultimate = random.randint(min, max)
+    if penultimate != 0:
+        result += f'{penultimate}*x + '
+    last = random.randint(min, max)
+    if last != 0:
+        result += f'{last}'
+    result += ' = 0'
+    return result
+
+first_polynominal = polynominal(n, 0, 100)
+second_polynominal = polynominal(n, 0, 100)
+print(first_polynominal)
+print(second_polynominal)
+
+data = open('first.txt', 'w')
+data.write(first_polynominal)
+data.close()
+
+data2 = open('second.txt', 'w')
+data2.write(second_polynominal)
+data2.close()
